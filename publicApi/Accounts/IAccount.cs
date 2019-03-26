b3c52f34a4b92276a60c9fb8f11c85f2ea7ebdc6
focus <focus@grids.tech>
2019-03-26 23:25:53 +0800

    clone

---
+++ publicApi/Accounts/IAccount.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace publicApi.Accounts
+{
+    public interface IAccount : ISerializable
+    {
+        /// <summary>
+        /// Set a property with data
+        /// </summary>
+        /// <param name="property">Must be one of the PROPERTY_ prefixed constants of \OCP\Accounts\IAccountManager</param>
+        /// <param name="value"></param>
+        /// <param name="scope"></param>
+        /// <param name="verified"></param>
+        /// <returns></returns>
+        IAccount setProperty(string property, string value, string
+            scope, string verified);
+
+        /// <summary>
+        /// Get a property by its key
+        /// </summary>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        IAccountProperty getProperty(string property);
+
+        /// <summary>
+        /// Get all properties of an account
+        /// </summary>
+        /// <returns></returns>
+        List<IAccountProperty> getProperties();
+
+        /// <summary>
+        /// Get all properties that match the provided filters for scope and verification status
+        /// </summary>
+        /// <param name="scope"></param>
+        /// <param name="verified"></param>
+        /// <returns></returns>
+        List<IAccountProperty> getFilteredProperties(string scope = "", string verified = "");
+
+        /// <summary>
+        /// Get the related user for the account data
+        /// </summary>
+        /// <returns></returns>
+        IUser getUser();
+    }
+}
---
+++ publicApi/Accounts/IAccountProperty.cs
@@ -0,0 +1,53 @@
+using System.Runtime.Serialization;
+
+namespace publicApi.Accounts
+{
+    public interface IAccountProperty : ISerializable
+    {
+        /// <summary>
+        /// Set the value of a property
+        /// @since 15.0.0
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        IAccountProperty setValue(string value);
+
+        /// <summary>
+        /// Set the scope of a property
+        /// </summary>
+        /// <param name="scope"></param>
+        /// <returns></returns>
+        IAccountProperty setScope(string scope);
+
+        /// <summary>
+        /// Set the verification status of a property
+        /// </summary>
+        /// <param name="verified"></param>
+        /// <returns></returns>
+        IAccountProperty setVerified(string verified);
+
+        /// <summary>
+        /// Get the name of a property
+        /// </summary>
+        /// <returns></returns>
+        string getName();
+
+        /// <summary>
+        /// Get the value of a property
+        /// </summary>
+        /// <returns></returns>
+        string getValue();
+
+        /// <summary>
+        /// Get the scope of a property
+        /// </summary>
+        /// <returns></returns>
+        string getScope();
+
+        /// <summary>
+        /// Get the verification status of a property
+        /// </summary>
+        /// <returns></returns>
+        string getVerified();
+    }
+}
