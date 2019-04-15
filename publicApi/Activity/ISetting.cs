aa9cc721c4bb7168d0132b272d3b66ae8572d7b2
focus <focus@grids.tech>
2019-04-15 22:44:42 +0800

    fix comment

--- publicApi/Activity/ISetting.cs
+++ publicApi/Activity/ISetting.cs
@@ -13,19 +13,19 @@ namespace publicApi.Activity
     interface ISetting
     {
 
-        /**
+        /*
          * @return string Lowercase a-z and underscore only identifier
          * @since 11.0.0
          */
         string getIdentifier();
 
-        /**
+        /*
          * @return string A translated string
          * @since 11.0.0
          */
         string getName();
 
-        /**
+        /*
          * @return int whether the filter should be rather on the top or bottom of
          * the admin section. The filters are arranged in ascending order of the
          * priority values. It is required to return a value between 0 and 100.
@@ -33,25 +33,25 @@ namespace publicApi.Activity
          */
         int getPriority();
 
-        /**
+        /*
          * @return bool True when the option can be changed for the stream
          * @since 11.0.0
          */
         bool canChangeStream();
 
-        /**
+        /*
          * @return bool True when the option can be changed for the stream
          * @since 11.0.0
          */
         bool isDefaultEnabledStream();
 
-        /**
+        /*
          * @return bool True when the option can be changed for the mail
          * @since 11.0.0
          */
         bool canChangeMail();
-
-        /**
+       
+        /*
          * @return bool True when the option can be changed for the stream
          * @since 11.0.0
          */
