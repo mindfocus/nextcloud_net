e9341500a61c46ce37f3eb335334411aa43e7416
focus <focus@grids.tech>
2019-04-16 14:29:11 +0800

    translate

---
+++ privateApi/Accounts/Account.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace privateApi.Accounts
+{
+    class Account : IAccount
+    {
+
+    /** @var IAccountProperty[] */
+    private $properties = [];
+
+	/** @var IUser */
+	private $user;
+
+	public function __construct(IUser $user)
+    {
+		$this->user = $user;
+    }
+
+    public function setProperty(string $property, string $value, string $scope, string $verified): IAccount {
+		$this->properties[$property] = new AccountProperty($property, $value, $scope, $verified);
+		return $this;
+
+    }
+
+    public function getProperty(string $property): IAccountProperty {
+		if (!array_key_exists($property, $this->properties)) {
+			throw new PropertyDoesNotExistException($property);
+}
+		return $this->properties[$property];
+
+    }
+
+public function getProperties(): array {
+		return $this->properties;
+
+    }
+
+public function getFilteredProperties(string $scope = null, string $verified = null): array {
+		return \array_filter($this->properties, function(IAccountProperty $obj) use($scope, $verified)
+{
+    if ($scope !== null && $scope !== $obj->getScope()) {
+        return false;
+    }
+    if ($verified !== null && $verified !== $obj->getVerified()) {
+        return false;
+    }
+    return true;
+});
+	}
+
+	public function jsonSerialize()
+{
+    return $this->properties;
+}
+
+public function getUser(): IUser {
+		return $this->user;
+
+    }
+}
+
+}
---
+++ publicApi/App/ManagerEvent.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace publicApi.App
+{
+    /**
+     * Class ManagerEvent
+     *
+     * @package OCP\APP
+     * @since 9.0.0
+     */
+    class ManagerEvent : Event
+    {
+
+	string EVENT_APP_ENABLE = @"OCP\App\IAppManager::enableApp";
+	const EVENT_APP_ENABLE_FOR_GROUPS = 'OCP\App\IAppManager::enableAppForGroups';
+	const EVENT_APP_DISABLE = 'OCP\App\IAppManager::disableApp';
+
+	/**
+	 * @since 9.1.0
+	 */
+	const EVENT_APP_UPDATE = 'OCP\App\IAppManager::updateApp';
+
+    /** @var string */
+    protected $event;
+    /** @var string */
+    protected $appID;
+	/** @var \OCP\IGroup[]|null */
+	protected $groups;
+
+	/**
+	 * DispatcherEvent constructor.
+	 *
+	 * @param string $event
+	 * @param $appID
+	 * @param \OCP\IGroup[]|null $groups
+	 * @since 9.0.0
+	 */
+	public function __construct($event, $appID, array $groups = null)
+    {
+		$this->event = $event;
+		$this->appID = $appID;
+		$this->groups = $groups;
+    }
+
+    /**
+	 * @return string
+	 * @since 9.0.0
+	 */
+    public function getEvent()
+    {
+        return $this->event;
+    }
+
+    /**
+	 * @return string
+	 * @since 9.0.0
+	 */
+    public function getAppID()
+    {
+        return $this->appID;
+    }
+
+    /**
+	 * returns the group Ids
+	 * @return string[]
+	 * @since 9.0.0
+	 */
+    public function getGroups()
+    {
+        return array_map(function($group) {
+            /** @var \OCP\IGroup $group */
+            return $group->getGID();
+        }, $this->groups);
+    }
+}
+
+}
