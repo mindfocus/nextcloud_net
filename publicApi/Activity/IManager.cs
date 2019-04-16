a33c4cb58c3de358977a1b3ebd74729bf6b99752
focus <focus@grids.tech>
2019-04-16 11:29:45 +0800

    translate

---
+++ publicApi/Activity/IManager.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace publicApi.Activity
+{
+    /**
+     * Interface IManager
+     *
+     * @package OCP\Activity
+     * @since 6.0.0
+     */
+    interface IManager
+    {
+        /**
+         * Generates a new IEvent object
+         *
+         * Make sure to call at least the following methods before sending it to the
+         * app with via the publish() method:
+         *  - setApp()
+         *  - setType()
+         *  - setAffectedUser()
+         *  - setSubject()
+         *
+         * @return IEvent
+         * @since 8.2.0
+         */
+        IEvent generateEvent();
+
+        /**
+         * Publish an event to the activity consumers
+         *
+         * Make sure to call at least the following methods before sending an Event:
+         *  - setApp()
+         *  - setType()
+         *  - setAffectedUser()
+         *  - setSubject()
+         *
+         * @param IEvent $event
+         * @throws \BadMethodCallException if required values have not been set
+         * @since 8.2.0
+         */
+        void publish(IEvent pEvent);
+
+	/**
+	 * In order to improve lazy loading a closure can be registered which will be called in case
+	 * activity consumers are actually requested
+	 *
+	 * $callable has to return an instance of \OCP\Activity\IConsumer
+	 *
+	 * @param \Closure $callable
+	 * @since 6.0.0
+	 */
+	void registerConsumer(Action callable);
+
+	/**
+	 * @param string $filter Class must implement OCA\Activity\IFilter
+	 * @since 11.0.0
+	 */
+	void registerFilter(string filter);
+
+	/**
+	 * @return IFilter[]
+	 * @since 11.0.0
+	 */
+	IList<IFilter> getFilters();
+
+	/**
+	 * @param string $id
+	 * @return IFilter
+	 * @throws \InvalidArgumentException when the filter was not found
+	 * @since 11.0.0
+	 */
+	IFilter getFilterById(string id);
+
+	/**
+	 * @param string $setting Class must implement OCA\Activity\ISetting
+	 * @since 11.0.0
+	 */
+	void registerSetting(string setting);
+
+	/**
+	 * @return ISetting[]
+	 * @since 11.0.0
+	 */
+	IList<ISetting> getSettings();
+
+	/**
+	 * @param string $provider Class must implement OCA\Activity\IProvider
+	 * @since 11.0.0
+	 */
+	void registerProvider(string provider);
+
+	/**
+	 * @return IProvider[]
+	 * @since 11.0.0
+	 */
+	IList<IProvider> getProviders();
+
+	/**
+	 * @param string $id
+	 * @return ISetting
+	 * @throws \InvalidArgumentException when the setting was not found
+	 * @since 11.0.0
+	 */
+	ISetting getSettingById(string id);
+
+	/**
+	 * @param string $type
+	 * @param int $id
+	 * @since 8.2.0
+	 */
+	void setFormattingObject(string type, int id);
+
+	/**
+	 * @return bool
+	 * @since 8.2.0
+	 */
+	bool isFormattingFilteredObject();
+
+	/**
+	 * @param bool $status Set to true, when parsing events should not use SVG icons
+	 * @since 12.0.1
+	 */
+	void setRequirePNG(bool status);
+
+	/**
+	 * @return bool
+	 * @since 12.0.1
+	 */
+	bool getRequirePNG();
+
+	/**
+	 * Set the user we need to use
+	 *
+	 * @param string|null $currentUserId
+	 * @throws \UnexpectedValueException If the user is invalid
+	 * @since 9.0.1
+	 */
+	void setCurrentUserId(string currentUserId = null);
+
+	/**
+	 * Get the user we need to use
+	 *
+	 * Either the user is logged in, or we try to get it from the token
+	 *
+	 * @return string
+	 * @throws \UnexpectedValueException If the token is invalid, does not exist or is not unique
+	 * @since 8.1.0
+	 */
+	string getCurrentUserId();
+}
+
+}
--- publicApi/UserInterface.cs
+++ publicApi/UserInterface.cs
@@ -1,9 +1,82 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace publicApi
 {
-    public interface UserInterface
+    /**
+     * TODO actually this is a IUserBackend
+     *
+     * @package OCP
+     * @since 4.5.0
+     */
+    interface UserInterface
     {
-        bool deleteUser(String uId);
+
+        /**
+         * Check if backend implements actions
+         * @param int $actions bitwise-or'ed actions
+         * @return boolean
+         *
+         * Returns the supported actions as int to be
+         * compared with \OC\User\Backend::CREATE_USER etc.
+         * @since 4.5.0
+         * @deprecated 14.0.0 Switch to the interfaces from OCP\User\Backend
+         */
+        bool implementsActions(Action actions);
+
+        /**
+         * delete a user
+         * @param string $uid The username of the user to delete
+         * @return bool
+         * @since 4.5.0
+         */
+        bool deleteUser(string uid);
+
+        /**
+         * Get a list of all users
+         *
+         * @param string $search
+         * @param null|int $limit
+         * @param null|int $offset
+         * @return string[] an array of all uids
+         * @since 4.5.0
+         */
+        IList<string> getUsers(string search = "", string limit = null, string offset = null);
+
+        /**
+         * check if a user exists
+         * @param string $uid the username
+         * @return boolean
+         * @since 4.5.0
+         */
+        bool userExists(string uid);
+
+        /**
+         * get display name of the user
+         * @param string $uid user ID of the user
+         * @return string display name
+         * @since 4.5.0
+         */
+        string getDisplayName(string uid);
+
+        /**
+         * Get a list of all display names and user ids.
+         *
+         * @param string $search
+         * @param string|null $limit
+         * @param string|null $offset
+         * @return array an array of all displayNames (value) and the corresponding uids (key)
+         * @since 4.5.0
+         */
+        IList<string> getDisplayNames(string search = "", int? limit = null, int? offset = null);
+
+        /**
+         * Check if a user list is available or not
+         * @return boolean if users can be listed or not
+         * @since 4.5.0
+         */
+        bool hasUserListings();
+
     }
+
 }
