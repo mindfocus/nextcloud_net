9cff50c254c1c519bb515ba8e85de6ca630daaf6
focus <focus@grids.tech>
2019-04-19 16:25:17 +0800

    add database model

---
+++ model/NCContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace model
+{
+    public class NCContext : DbContext
+    {
+        public DbSet<AccountTable> Accounts { get; set; }
+    }
+}
