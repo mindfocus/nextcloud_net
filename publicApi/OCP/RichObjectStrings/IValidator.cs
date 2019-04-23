158e1f1a44adfa8002f8c175215dbd59cc0dd3b8
focus <focus@grids.tech>
2019-04-23 10:09:51 +0800

    translate

---
+++ publicApi/OCP/RichObjectStrings/IValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCP.RichObjectStrings
+{
+    /**
+     * Class Validator
+     *
+     * @package OCP\RichObjectStrings
+     * @since 11.0.0
+     */
+    public interface IValidator
+    {
+
+        /**
+         * @param string subject
+         * @param array[] parameters
+         * @throws InvalidObjectExeption
+         * @since 11.0.0
+         */
+        void validate(string subject, IList<string> parameters);
+    }
+
+}
---
+++ publicApi/OCP/RichObjectStrings/InvalidObjectExeption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCP.RichObjectStrings
+{
+    /**
+     * Class InvalidObjectExeption
+     *
+     * @package OCP\RichObjectStrings
+     * @since 11.0.0
+     */
+    class InvalidObjectExeption : InvalidArgumentException {
+}
+
+}
