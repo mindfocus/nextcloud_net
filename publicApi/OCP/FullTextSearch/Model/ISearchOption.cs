3a7a0001d8e9247e29c5e65d393a87a4ebb35041
focus <focus@grids.tech>
2019-04-24 00:24:56 +0800

    translate

---
+++ publicApi/OCP/FullTextSearch/Model/ISearchOption.cs
@@ -0,0 +1,144 @@
+﻿using System;
+namespace OCP.FullTextSearch.Model
+{
+    /**
+     * @since 16.0.0
+     *
+     * Interface ISearchOption
+     *
+     * @package OCP\FullTextSearch\Model
+     */
+    interface ISearchOption
+    {
+
+
+        /**
+         * @since 16.0.0
+         */
+        const CHECKBOX = 'checkbox';
+
+        /**
+         * @since 16.0.0
+         */
+        const INPUT = 'input';
+
+        /**
+         * @since 16.0.0
+         */
+        const INPUT_SMALL = 'small';
+
+
+        /**
+         * Set the name/key of the option.
+         * The string should only contains alphanumerical chars and underscore.
+         * The key can be retrieve when using ISearchRequest::getOption
+         *
+         * @see ISearchRequest::getOption
+         *
+         * @since 16.0.0
+         *
+         * @param string name
+         *
+         * @return ISearchOption
+         */
+        public function setName(string name): ISearchOption;
+
+    /**
+     * Get the name/key of the option.
+     *
+     * @since 16.0.0
+     *
+     * @return string
+     */
+    public function getName(): string;
+
+
+    /**
+     * Set the title/display name of the option.
+     *
+     * @since 16.0.0
+     *
+     * @param string title
+     *
+     * @return ISearchOption
+     */
+    public function setTitle(string title): ISearchOption;
+
+    /**
+     * Get the title of the option.
+     *
+     * @since 16.0.0
+     *
+     * @return string
+     */
+    public function getTitle(): string;
+
+
+    /**
+     * Set the type of the option.
+     * type can be ISearchOption::CHECKBOX or ISearchOption::INPUT
+     *
+     * @since 16.0.0
+     *
+     * @param string type
+     *
+     * @return ISearchOption
+     */
+    public function setType(string type): ISearchOption;
+
+    /**
+     * Get the type of the option.
+     *
+     * @since 16.0.0
+     *
+     * @return string
+     */
+    public function getType(): string;
+
+
+    /**
+     * In case of Type is INPUT, set the size of the input field.
+     * Value can be ISearchOption::INPUT_SMALL or not defined.
+     *
+     * @since 16.0.0
+     *
+     * @param string size
+     *
+     * @return ISearchOption
+     */
+    public function setSize(string size): ISearchOption;
+
+    /**
+     * Get the size of the INPUT.
+     *
+     * @since 16.0.0
+     *
+     * @return string
+     */
+    public function getSize(): string;
+
+
+    /**
+     * In case of Type is , set the placeholder to be displayed in the input
+     * field.
+     *
+     * @since 16.0.0
+     *
+     * @param string placeholder
+     *
+     * @return ISearchOption
+     */
+    public function setPlaceholder(string placeholder): ISearchOption;
+
+    /**
+     * Get the placeholder.
+     *
+     * @since 16.0.0
+     *
+     * @return string
+     */
+    public function getPlaceholder(): string;
+
+}
+
+}
---
+++ publicApi/OCP/FullTextSearch/Model/ISearchRequest.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OCP.FullTextSearch.Model
+{
+    /**
+     * Interface ISearchRequest
+     *
+     * When a search request is initiated, from a request from the front-end or using
+     * the IFullTextSearchManager::search() method, FullTextSearch will create a
+     * SearchRequest object, based on this interface.
+     *
+     * The object will be passed to the targeted Content Provider so it can convert
+     * search options using available method.
+     *
+     * The object is then encapsulated in a SearchResult and send to the
+     * Search Platform.
+     *
+     * @since 15.0.0
+     *
+     *
+     * @package OCP\FullTextSearch\Model
+     */
+    interface ISearchRequest
+    {
+
+
+        /**
+         * Get the maximum number of results to be returns by the Search Platform.
+         *
+         * @since 15.0.0
+         *
+         * @return int
+         */
+        int getSize();
+
+
+        /**
+         * Get the current page.
+         * Used by pagination.
+         *
+         * @since 15.0.0
+         *
+         * @return int
+         */
+        int getPage();
+
+
+        /**
+         * Get the author of the request.
+         *
+         * @since 15.0.0
+         *
+         * @return string
+         */
+        string getAuthor();
+
+        /**
+         * Get the searched string.
+         *
+         * @since 15.0.0
+         *
+         * @return string
+         */
+        string getSearch();
+
+
+        /**
+         * Get the value of an option (as string).
+         *
+         * @since 15.0.0
+         *
+         * @param string option
+         * @param string default
+         *
+         * @return string
+         */
+        string getOption(string option, string @default = "");
+
+    /**
+     * Get the value of an option (as array).
+     *
+     * @since 15.0.0
+     *
+     * @param string option
+     * @param array default
+     *
+     * @return array
+     */
+    IList<string> getOptionArray(string option, IList<string> @default);
+
+
+        /**
+         * Limit the search to a part of the document.
+         *
+         * @since 15.0.0
+         *
+         * @param string part
+         *
+         * @return ISearchRequest
+         */
+        ISearchRequest addPart(string part) ;
+
+        /**
+         * Limit the search to an array of parts of the document.
+         *
+         * @since 15.0.0
+         *
+         * @param array parts
+         *
+         * @return ISearchRequest
+         */
+        ISearchRequest setParts(IList<string> parts) ;
+
+        /**
+         * Get the parts the search is limited to.
+         *
+         * @since 15.0.0
+         *
+         * @return array
+         */
+        IList<string> getParts();
+
+
+        /**
+         * Limit the search to a specific meta tag.
+         *
+         * @since 15.0.0
+         *
+         * @param string tag
+         *
+         * @return ISearchRequest
+         */
+        ISearchRequest addMetaTag(string tag);
+
+    /**
+     * Get the meta tags the search is limited to.
+     *
+     * @since 15.0.0
+     *
+     * @return array
+     */
+    IList<string> getMetaTags();
+
+        /**
+         * Limit the search to an array of meta tags.
+         *
+         * @since 15.0.0
+         *
+         * @param array tags
+         *
+         * @return ISearchRequest
+         */
+        ISearchRequest setMetaTags(IList<string> tags) ;
+
+
+        /**
+         * Limit the search to a specific sub tag.
+         *
+         * @since 15.0.0
+         *
+         * @param string source
+         * @param string tag
+         *
+         * @return ISearchRequest
+         */
+        ISearchRequest addSubTag(string source, string tag);
+
+    /**
+     * Get the sub tags the search is limited to.
+     *
+     * @since 15.0.0
+     *
+     * @param bool formatted
+     *
+     * @return array
+     */
+    IList<string> getSubTags(bool formatted);
+
+        /**
+         * Limit the search to an array of sub tags.
+         *
+         * @since 15.0.0
+         *
+         * @param array tags
+         *
+         * @return ISearchRequest
+         */
+        ISearchRequest setSubTags(IList<string> tags) ;
+
+
+        /**
+         * Limit the search to a specific field of the mapping, using a full string.
+         *
+         * @since 15.0.0
+         *
+         * @param string field
+         *
+         * @return ISearchRequest
+         */
+        ISearchRequest addLimitField(string field) ;
+
+    /**
+     * Get the fields the search is limited to.
+     *
+     * @since 15.0.0
+     *
+     * @return array
+     */
+    IList<string> getLimitFields();
+
+
+        /**
+         * Limit the search to a specific field of the mapping, using a wildcard on
+         * the search string.
+         *
+         * @since 15.0.0
+         *
+         * @param string field
+         *
+         * @return ISearchRequest
+         */
+        ISearchRequest addWildcardField(string field);
+
+    /**
+     * Get the limit to field of the mapping.
+     *
+     * @since 15.0.0
+     *
+     * @return array
+     */
+    IList<string> getWildcardFields();
+
+
+        /**
+         * Filter the results, based on a group of field, using regex
+         *
+         * @since 15.0.0
+         *
+         * @param array filters
+         *
+         * @return ISearchRequest
+         */
+        ISearchRequest addRegexFilters(IList<string> filters) ;
+
+        /**
+         * Get the regex filters the search is limit to.
+         *
+         * @since 15.0.0
+         *
+         * @return array
+         */
+        IList<string> getRegexFilters();
+
+
+        /**
+         * Filter the results, based on a group of field, using wildcard
+         *
+         * @since 15.0.0
+         *
+         * @param array filter
+         *
+         * @return ISearchRequest
+         */
+        ISearchRequest addWildcardFilter(IList<string> filter);
+
+        /**
+         * Get the wildcard filters the search is limit to.
+         *
+         * @since 15.0.0
+         *
+         * @return array
+         */
+        IList<string> getWildcardFilters();
+
+
+        /**
+         * Add an extra field to the search.
+         *
+         * @since 15.0.0
+         *
+         * @param string field
+         *
+         * @return ISearchRequest
+         */
+        ISearchRequest addField(string field);
+
+    /**
+     * Get the list of extra field to search into.
+     *
+     * @since 15.0.0
+     *
+     * @return array
+     */
+    IList<string> getFields();
+
+
+}
+
+
+}
